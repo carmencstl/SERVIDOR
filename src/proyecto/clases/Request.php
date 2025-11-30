8e8d28b931e4efc317b051b8426fdb519fd41589
Carmen <ccastillogaitan12@gmail.com>
2025-11-30 19:33:19 +0100

    nuevo

--- src/proyecto/clases/Request.php
+++ src/proyecto/clases/Request.php
@@ -5,6 +5,10 @@
         private function __construct()
         {}
 
+        /**
+         * @param string $url
+         * @return never
+         */
         public static function redirect(string $url): never
         {
             header("Location: $url");
--- src/proyecto/clases/Sesion.php
+++ src/proyecto/clases/Sesion.php
@@ -1,22 +1,64 @@
 <?php
 
-    require_once "../clases/Request.php";
-    class Sesion
-    {
-        #HACERLO SINGLETON
-//        public static function login($usuario)
+    require_once "clases/Usuario.php";
+    final class Sesion {
+        private static $instance = null;
+        private ?Usuario $usuario;
 
+        private function __construct() {
+            session_start();
+            if (isset($_SESSION["usuarioActivo"])) {
+                $this->usuario = $_SESSION["usuarioActivo"];
+            }
+        }
 
+        /**
+         * @return void
+         *
+         */
+        private function __clone() {}
 
 
         /**
-         * @return never
+         * @return Sesion
+         *
          */
-        public static function cerrarSesion(): never
-        {
-            session_start();
-            $_SESSION = [];
+        public static function getInstance() {
+            if (self::$instance === null) {
+                self::$instance = new Sesion();
+            }
+            return self::$instance;
+        }
+
+        /**
+         * @param Usuario $usuario
+         * @return void
+         *
+         */
+        public function iniciarSesion(Usuario $usuario) {
+            $this->usuario = $usuario;
+            $_SESSION["usuarioActivo"] = $usuario;
+        }
+
+        /**
+         * @return void
+         */
+        public function cerrarSesion() {
+            $this->usuario = null;
             session_destroy();
-            Request::redirect("login.php");
+        }
+
+        /**
+         * @return bool
+         */
+        public function estaLogueado() {
+            return $this->usuario !== null;
+        }
+
+        /**
+         * @return Usuario|null
+         */
+        public function getUsuario() {
+            return $this->usuario;
         }
     }
